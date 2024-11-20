b07c134a99164eb80f3a838487aa16de95ef6690
Edmond <edmondchu10@hotmail.com>
2024-11-20 20:52:47 +0000

    Change drawing feature to draw only when the user holds down click

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ const grid = document.querySelector('.grid');
 const resize = document.querySelector('#resize');
 const colourBtn = document.querySelector('#random-colour');
 let colour  = randomColour();
+let mouseHold = false;
+
+document.body.onmousedown = (event) => {
+    mouseHold = true;
+    event.preventDefault();
+};
+document.body.onmouseup = () => {mouseHold = false};
+
 // Initial Grid
 drawGrid(16,16);
 
+// Event Listeners
 resize.addEventListener('click',() => {
     let squares = prompt('How many squares do you want on each side?');
     clearGrid();
     drawGrid(squares);
 })
 
-grid.addEventListener('mouseover',(event) =>{
-    event.target.setAttribute('style',`background-color: ${colour};`);
-})
+grid.addEventListener('mousedown',colourCell);
+grid.addEventListener('mouseover',colourCell);
 
 colourBtn.addEventListener('click', () => {
     colour = randomColour();
 })
 
+// Functions
 function clearGrid() {
     grid.innerHTML = '';
 }
@@ -34,7 +43,6 @@ function drawGrid(squares) {
         }
         grid.appendChild(row);
     }
-    
 }
 
 function randomColour() {
@@ -42,4 +50,9 @@ function randomColour() {
     let g = Math.floor(Math.random()*256);
     let b = Math.floor(Math.random()*256);
     return `rgb(${r},${g},${b})`
+}
+
+function colourCell(e) {
+    if (e.type === 'mouseover' && !mouseHold) return
+    e.target.setAttribute('style',`background-color: ${colour};`);
 }
